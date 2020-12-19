2c98a55d61145e49b06ee2fe68839746201a4ee4
Elvis Rodriguez Duran <animeamv682@gmail.com>
2020-12-19 15:24:14 -0300

    se arreglo la transicion entres ventanas

--- src/interfaz.java
+++ src/interfaz.java
@@ -1,6 +1,70 @@
 
-public class interfaz {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+
+public class interfaz{
+
+ private JPanel panel;
+ private JLabel	titulo;
+ private JTextArea ingresar_matriz;
+ private JButton resolver;
+ private boolean permiso = false;
+ 
 	
+	public interfaz() {
+		
+		panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBackground(new Color(233, 240, 246));
+
+		titulo = new JLabel("INGRESE LA BASE DEL SUDOKU");
+		titulo.setBounds(40,30,300,20);
+		//titulo.setHorizontalAlignment(JLabel.CENTER);
+		titulo.setFont(new Font("", Font.BOLD, 18));
+		
+		ingresar_matriz = new JTextArea();
+		ingresar_matriz.setBounds(80, 80, 220, 350);
+		ingresar_matriz.setEditable(true);
+		ingresar_matriz.setLineWrap(true);
+		ingresar_matriz.setFont(new Font("", Font.BOLD,29));
+		//ingresar_matriz.setWrapStyleWord(true);
+		ingresar_matriz.setBackground(new Color(233, 240, 248));
+		
+		resolver = new JButton("Resolver");
+		resolver.setBounds(120, 450, 120, 40);
+		
+	
+		panel.add(titulo);
+		panel.add(ingresar_matriz);
+		panel.add(resolver);
+		
+	    resolver.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				permiso = true;
+				panel.setVisible(false);	
+		
+			}
+			
+		});
+		
+			
+	}
+	
+	public JPanel getPanel() {
+		return panel;
+	}
+	public boolean getPermiso() {
+		return permiso;
+	}
 	
 }
--- src/main.java
+++ src/main.java
@@ -1,12 +1,54 @@
+import javax.swing.JFrame;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		JFrame ventana1 = new JFrame();
+		ventana1.setSize(400,550);
+		ventana1.setTitle("Verificador de Sudoku");
+		ventana1.setResizable(false);
+		ventana1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+        funcion(ventana1);
+	
+		
 	}
 	
 	public boolean resolver(){
 		return true;
 	}
+	
+	public static void funcion(JFrame ventana1) {
+		
+		interfaz window = new interfaz();
+		ventana1.add(window.getPanel());
+		
+		ventana1.setVisible(true);
+		
+		while(!window.getPermiso()) {
+			
+			System.out.println(".");
+			
+		}
+		
+		interfaz2 window2 = new interfaz2();
+		ventana1.add(window2.getPanel());
+		
+		ventana1.setVisible(true);
+		
+        while(!window2.getPermiso()) {
+			
+			System.out.println(".");
+			
+		}
+		
+        funcion(ventana1);
+		
+	}
+	
+	
+	
+	
 
 }
