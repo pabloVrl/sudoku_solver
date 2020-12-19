668793ce5d3ec4fb2cb969a1f32491cb6a79f503
Elvis Rodriguez Duran <animeamv682@gmail.com>
2020-12-19 18:52:05 -0300

    se creo la interfaz para el programa

--- src/interfaz.java
+++ src/interfaz.java
@@ -8,50 +8,90 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 
 public class interfaz{
 
  private JPanel panel;
  private JLabel	titulo;
+ private JLabel titulo2;
+ private JTextArea linea;
  private JTextArea ingresar_matriz;
+ private JTextArea respuesta;
  private JButton resolver;
- private boolean permiso = false;
+ private JButton reset;
+ private final Color fondo = new Color(200, 218, 211);
  
-	
+ 
 	public interfaz() {
 		
 		panel = new JPanel();
 		panel.setLayout(null);
-		panel.setBackground(new Color(233, 240, 246));
-
+		panel.setBackground(fondo);
+		
+		//VENTANA1
 		titulo = new JLabel("INGRESE LA BASE DEL SUDOKU");
-		titulo.setBounds(40,30,300,20);
-		//titulo.setHorizontalAlignment(JLabel.CENTER);
-		titulo.setFont(new Font("", Font.BOLD, 18));
+		titulo.setBounds(20,35,400,20);
+		titulo.setFont(new Font("", Font.BOLD, 22));
 		
 		ingresar_matriz = new JTextArea();
 		ingresar_matriz.setBounds(80, 80, 220, 350);
 		ingresar_matriz.setEditable(true);
 		ingresar_matriz.setLineWrap(true);
 		ingresar_matriz.setFont(new Font("", Font.BOLD,29));
-		//ingresar_matriz.setWrapStyleWord(true);
-		ingresar_matriz.setBackground(new Color(233, 240, 248));
+		ingresar_matriz.setBackground(fondo);
 		
-		resolver = new JButton("Resolver");
+		resolver = new JButton("RESOLVER");
 		resolver.setBounds(120, 450, 120, 40);
+		resolver.setBorderPainted(true);
+		resolver.setFocusPainted(false);
+		resolver.setContentAreaFilled(true);
+		resolver.setBackground(Color.white);
+		
+		//LINEA
+		linea = new JTextArea();
+		linea.setEditable(false);
+		linea.setBounds(390,0,10,800);
+		linea.setBackground(new Color(147, 181, 179));
+		
+		
+		//VENTANA2
+		
+		titulo2 = new JLabel("SOLUCIÓN");
+		titulo2.setBounds(530, 35,300 , 22);
+		titulo2.setFont(new Font("", Font.BOLD, 20));
+		
+        respuesta = new JTextArea();
+        respuesta.setBounds(480, 80, 220, 350);
+        respuesta.setEditable(false);
+		respuesta.setFont(new Font("", Font.BOLD, 29));
+		respuesta.setBackground(fondo);
+		
+		
+		reset = new JButton("LIMPIAR");
+		reset.setBounds(530, 450, 120, 40);
+		reset.setBorderPainted(true);
+		reset.setFocusPainted(false);
+		reset.setContentAreaFilled(true);
+		reset.setBackground(Color.white);
+        
 		
 	
 		panel.add(titulo);
 		panel.add(ingresar_matriz);
 		panel.add(resolver);
+		panel.add(linea);
+		panel.add(titulo2);
+		panel.add(respuesta);
+		panel.add(reset);
 		
 	    resolver.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent e) {
 				
-				permiso = true;
-				panel.setVisible(false);	
+				
+				
 		
 			}
 			
@@ -62,9 +102,6 @@ public class interfaz{
 	
 	public JPanel getPanel1() {
 		return panel;
-	}
-	public boolean getPermiso1() {
-		return permiso;
 	}
 	
 }
--- src/main.java
+++ src/main.java
@@ -7,7 +7,7 @@ public class main {
 
 		
 		JFrame ventana1 = new JFrame();
-		ventana1.setSize(400,550);
+		ventana1.setSize(800,550);
 		ventana1.setTitle("Verificador de Sudoku");
 		ventana1.setResizable(false);
 		ventana1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,11 +15,13 @@ public class main {
         Sudoku s = new Sudoku();
 		
 		s.resolver();
-		
 		s.mostrar();
 		
 		
-        funcion(ventana1);
+		interfaz window = new interfaz();
+		ventana1.add(window.getPanel1());
+		
+		ventana1.setVisible(true);
 	
 		
 	}
@@ -28,33 +30,7 @@ public class main {
 		return true;
 	}
 	
-	public static void funcion(JFrame ventana1) {
-		
-		interfaz window = new interfaz();
-		ventana1.add(window.getPanel1());
-		
-		ventana1.setVisible(true);
-		
-		while(!window.getPermiso1()) {
-			
-		System.out.println(".");
-			
-		}
-		
-		interfaz2 window2 = new interfaz2();
-		ventana1.add(window2.getPanel2());
-		
-		ventana1.setVisible(true);
-		
-        while(!window2.getPermiso2()) {
-			
-			System.out.println(".");
-			
-		}
-		
-        funcion(ventana1);
-		
-	}
+	
 		
 }
 	
