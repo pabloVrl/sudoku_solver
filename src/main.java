a696671c7d218257a6a992b9b916d9e6ca294f1e
Pablo Villarroel <pablovillarroel135@gmail.com>
2020-12-22 19:34:24 -0300

    se agregaron nombre y rut de los creadores

--- src/main.java
+++ src/main.java
@@ -1,5 +1,10 @@
 
 import javax.swing.JFrame;
+//PROYECTO CREADO POR:
+//PABLO VILLARROEL ANTILLANCA 20.487.087-K
+//ELVIS RODRIGUEZ DURAN 20.255.414-8
+//
+//INGENIERIA DE EJECUCIÓN EN COMPUTACIÓN E INFORMÁTICA
 public class main {
 
 	public static void main(String[] args) {
